530e4cdcd6b02d40f9ebcdfbf1c749d40e53f44b
Anatole Cadet <anatolec@hotmail.com>
2024-04-15 18:04:19 -0400

    Erase unuse comment in main.tf

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "route_table_association" {
   source         = "./modules/route_table_association"
   count          = var.number_subnet
   subnet_id      = module.subnet_module.subnet_output[count.index].id
-  route_table_id = module.route_table.route_table_id_output[count.index] #module.route_table.route_table_output[count.index].id
+  route_table_id = module.route_table.route_table_id_output[count.index] 
 }
 
 
@@ -79,7 +79,7 @@ module "ec2_instance_module" {
   instance_type       = var.instance_type
   subnet_list         = module.subnet_module.subnet_output
   number_ec2_instance = var.number_ec2_instance
-  security_group_list = module.security_group_module.security_group_id_output #module.security_group_module.security_group_list_output
+  security_group_list = module.security_group_module.security_group_id_output
   environment         = var.environment
 }
 
